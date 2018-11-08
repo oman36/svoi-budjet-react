ffdae3b595888f24f9533f1274422ec0ef8d5881
Petrov Vladimir <neoman36@gmail.com>
2018-11-08 14:12:58 +0300

    ProductsAPI was added

---
+++ src/Api/v1/ProductsAPI.js
@@ -0,0 +1,33 @@
+import {entrypoint, buildIncludes, get} from '../API';
+
+export const ProductsAPI = {
+    all: function (params) {
+        const url = new URL('v1/products', entrypoint);
+        [
+            'offset',
+            'limit',
+            'sort_by',
+        ].forEach((key) => {
+            if (key in params) url.searchParams.append(key, params[key])
+        });
+
+        if ('include' in params) {
+            url.searchParams.append('include', buildIncludes(params.include))
+        }
+        return get(url);
+    },
+    items: function (id, params) {
+        const url = new URL(`v1/products/${id}/items`, entrypoint);
+        [
+            'offset',
+            'limit',
+            'sort_by',
+        ].forEach((key) => {
+            if (key in params) url.searchParams.append(key, params[key])
+        });
+        if ('include' in params) {
+            url.searchParams.append('include', buildIncludes(params.include))
+        }
+        return get(url);
+    },
+};
